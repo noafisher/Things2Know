c9b271333509534bfbec71e3c111828271751131
User <User@LAB4-05>
2023-11-14 10:43:43 +0200

    Add project files.

---
+++ Things2Know/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Things2Know
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student();
+            student.Name = "Test";
+            Console.WriteLine(  student.Name);
+            student.Name = "";
+            Console.WriteLine(student.Name);
+            object p = student;
+            object p1 = student.Name;
+            Train<Student> train = new Train<Student>();
+            Train<string> train1 = new Train<string>();
+            train.count = 4;
+            train1.count = 5;  
+            List<string> list = new List<string>();
+            List<Student> students = new List<Student>();
+            //List<Train<Student>> trains = new List<Train<Student>>();
+        
+            students.Add(student);
+            students.Add(new Student() { Name = "Noa"});
+            Console.WriteLine(students[0].Name);
+            students.Remove(student);
+            students.RemoveAt(0);
+
+
+            for (int i = 0; i < students.Count; i++)
+            {
+
+            }
+           
+
+
+        }
+    }
+}
---
+++ Things2Know/Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Things2Know
+{
+    public class Student
+    {
+        private string name;
+        public string Name { get { return name; } set { if (!string.IsNullOrEmpty(value)) name = value; } }
+
+
+        public string LastName { get; protected set; } = "Kuku";  
+    }
+
+    public class  Train<Z>
+    {
+        Z[] passangers;
+        public int count { get; set; } = 0;
+    }
+}
